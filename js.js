303b1e388a21dc1917c2622343ddf90e5991acbc
SangLe0211 <leducsang2001@gmail.com>
2021-09-28 18:39:39 +0700

    demo2
    sssss

---
+++ js.js
@@ -0,0 +1,58 @@
+//   -------js for MenuToggle------------------
+
+  
+  var  MenuItems = document.getElementById('MenuItems');
+
+  MenuItems.style.maxWidth = "0%";
+  function menutoggle(){
+
+      if(MenuItems.style.maxWidth == "0%"){
+
+        MenuItems.style.maxWidth = "100%";
+      }
+      else{
+
+        MenuItems.style.maxWidth = "0%";
+      }
+
+  }
+  // function close_menu(){
+  //   document.getElementById('MenuItems_close').style.display = "none";
+  // }
+  
+  // function close_menu(){
+  //   if(window.width < "800px" ){
+  //     MenuItems.style.maxWidth = "0%";
+  //   }
+  //   else{
+  //     MenuItems.style.maxWidth = "100%";
+  //   }
+  // }
+// //   ----------------------js for product-----------------
+
+
+
+    var ProductImg = document.getElementById("ProductImg")
+    var SmallImg = document.getElementsByClassName("small-img")
+
+    SmallImg[0].onclick = function()
+    {
+        ProductImg.src = SmallImg[0].src
+    }
+    SmallImg[1].onclick = function()
+    {
+        ProductImg.src = SmallImg[1].src
+    }
+    SmallImg[2].onclick = function()
+    {
+        ProductImg.src = SmallImg[2].src
+    }
+    SmallImg[3].onclick = function()
+    {
+        ProductImg.src = SmallImg[3].src
+    }
+  
+
+    // ----scroll----
+    
+ 
